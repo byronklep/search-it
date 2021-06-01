b4934077a70af245891305b18a9e742df886d241
byronklep <byronklep@gmail.com>
2021-06-01 11:04:30 -0500

    Card styling.

--- src/components/home/components/ListItem.js
+++ src/components/home/components/ListItem.js
@@ -6,16 +6,16 @@ const ListItem = ({ fungus }) => {
       <div className="card mb-4 grow">
         <div className="card-body">
           <a href={fungus.url}>
-            <h1>{fungus.title}</h1>
+            <h1 className="ml-3">{fungus.title}</h1>
           </a>
 
           <ListGroup variant="flush">
             <ListGroupItem className="card-details">
-              {fungus.description}
+              <a className="text-success" href={fungus.url}>
+                {fungus.url}{' '}
+              </a>
             </ListGroupItem>
-            <ListGroupItem>
-              <a href={fungus.url}>{fungus.url} </a>
-            </ListGroupItem>
+            <ListGroupItem>{fungus.description}</ListGroupItem>
           </ListGroup>
         </div>
       </div>
