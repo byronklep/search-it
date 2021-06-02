c8165a600f60c92a60fed582ebadf8e44af9ff2b
byronklep <byronklep@gmail.com>
2021-06-01 23:30:24 -0500

    Image search error handling.

--- src/components/image/components/ImageListItem.js
+++ src/components/image/components/ImageListItem.js
@@ -6,7 +6,12 @@ const ListItem = ({ fungus }) => {
       <Container className="image-search">
         <a href={fungus.url}>
           <img
-            src={fungus.thumbnail}
+            src={fungus.url}
+            onError={(e) => {
+              e.target.src =
+                'https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg'
+              e.target.onError = null
+            }}
             alt={fungus.title}
             height={200}
             width={250}
